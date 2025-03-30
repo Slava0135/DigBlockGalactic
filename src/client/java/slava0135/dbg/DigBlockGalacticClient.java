59295f3f67f3178452d32e50168973d8dc07e8f6
slava0135 <super.novalskiy_0135@inbox.ru>
2025-03-30 12:46:16 +0300

    init

---
+++ src/client/java/slava0135/dbg/DigBlockGalacticClient.java
@@ -0,0 +1,10 @@
+package slava0135.dbg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DigBlockGalacticClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
